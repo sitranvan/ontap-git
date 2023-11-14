2e76df8d3a2ff19e06985159d7e2175276fa1d20
Si Tran Van <it.sitranvan@gmail.com>
2023-11-14 18:41:19 +0700

    hamsonguyento

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-
+#include<cmath>
 using namespace std;
 
 // Hàm nhập một số nguyên
@@ -7,6 +7,20 @@ int nhapSoNguyen(int n) {
 	cout<<"Nhap so nguyen n: ";
 	cin>>n;
 	return n;
+}
+
+// Kiểm tra số nguyên tố
+bool ktNguyenTo(int n) {
+	if (n < 2) {
+		return false;
+	}
+	for (int i = 2; i <= sqrt(n); i++) {
+		if (n % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
 }
 
 int main() {
